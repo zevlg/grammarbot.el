d6ccf53c5eb3aced09c501c97a36219dd10b9866
Zajcev Evgeny <zevlg@yandex.ru>
2020-02-01 03:09:36 +0300

    * Bug fixes, new custom options
    * README.org regenerated

--- grammarbot.el
+++ grammarbot.el
@@ -51,6 +51,9 @@
 
 ;; * Customizable Options
 ;;
+;; You can customize =grammarbot.el= with {{{kbd(M-x customize-group RET grammarbot RET)}}} using Emacs [[https://www.gnu.org/software/emacs/manual/html_node/emacs/Easy-Customization.html#Easy-Customization][Easy Customization Interface]]
+;;
+;; Or just tweak following user options in your =init.el=:
 ;; - User Option: ~grammarbot-language~
 ;;
 ;;   {{{vardoc1(grammarbot-language)}}}
@@ -62,6 +65,19 @@
   "Language to use."
   :type '(choice (const :tag "American English" "en-US")
                  (const :tag "British English" "en-GB"))
+  :group 'grammarbot)
+
+;; - User Option: ~grammarbot-show-stats~, default: {{{eval(grammarbot-show-stats)}}}
+;;
+;;   {{{vardoc(grammarbot-show-stats)}}}
+;;
+;;   Stats are shown in echo area with message like:
+;;   #+BEGIN_EXAMPLE
+;;   Grammarbot issues: 2, ignored: 1, replaced: 1
+;;   #+END_EXAMPLE
+(defcustom grammarbot-show-stats t
+  "Non-nil to show statistics for all found issues after check."
+  :type 'boolean
   :group 'grammarbot)
 
 ;; - User Option: ~grammarbot-api-key~
@@ -81,6 +97,30 @@ Without API Key there is a limit up to 100 requests per day."
 ;; - User Option: ~grammarbot-accept-single-choice-rules~
 ;;
 ;;   {{{vardoc1(grammarbot-accept-single-choice-rules)}}}
+;;
+;;   Consider we are checking next sentence with =grammarbot.el=:
+;;   #+BEGIN_EXAMPLE
+;;   However it is wise to automatically accept single replacements
+;;   #+END_EXAMPLE
+;;
+;;   While checking, =*Grammarbot Choices*= buffer pops up for the issue:
+;;   #+BEGIN_EXAMPLE
+;;   Did you forget a comma after a conjunctive/linking adverb?
+;;   Rule: (:id "SENT_START_CONJUNCTIVE_LINKING_ADVERB_COMMA" :subId "1" :description "Commas after conjunctive/linking adverbs in front of a new sentence." :issueType "typographical" :urls [(:value "https://writing.wisc.edu/Handbook/ConjAdv.html")] :category (:id "PUNCTUATION" :name "Punctuation"))
+;;
+;;   (0) However,
+;;   #+END_EXAMPLE
+;;
+;;   As you can see, issue has single replacement - "However,".  For
+;;   single replacement issues corresponding issue rule is shown.
+;;
+;;   You can customize ~grammarbot-accept-single-choice-rules~ to
+;;   automatically accept such issue and do the auto-replacement.  To
+;;   do so, add:
+;;   #+BEGIN_SRC emacs-lisp
+;;   (add-to-list 'grammarbot-accept-single-choice-rules
+;;                '(:issueType "typographical" :category (:id "PUNCTUATION")))
+;;   #+END_SRC
 (defcustom grammarbot-accept-single-choice-rules nil
   "List of rules for issues to automatically accept single choice replacement.
 Each match has `:rule' property.  If any of element in
@@ -94,6 +134,24 @@ Examples of elements:
   :options '(((:issueType "grammar" :category (:id "GRAMMAR"))))
   :group 'grammarbot)
 
+;; - User Option: ~grammarbot-ignore-single-choice-rules~
+;;
+;;   {{{vardoc1(grammarbot-ignore-single-choice-rules)}}}
+;;
+;;   Elements of the list are in same format as fors
+;;   ~grammarbot-accept-single-choice-rules~.
+;;
+;;   By default "Whitespace repetition" and "Sentence starts with an
+;;   uppercase letter" issues are ignored.
+(defcustom grammarbot-ignore-single-choice-rules
+  '((:id "WHITESPACE_RULE" :description "Whitespace repetition (bad formatting)" :issueType "whitespace" :category (:id "TYPOGRAPHY"))
+    (:id "UPPERCASE_SENTENCE_START" :description "Checks that a sentence starts with an uppercase letter" :issueType "typographical" :category (:id "CASING")))
+  "List of rules for issues to automatically ignore single choice replacement.
+Elements are same as for `grammarbot-accept-single-choice-rules'."
+  :type 'list
+  :group 'grammarbot)
+
+
 (defconst grammarbot-api-url "http://api.grammarbot.io/v2/check"
   "API URL to grammarbot service.")
 
@@ -193,7 +251,7 @@ Return nil or replacement index number."
 
 (defun grammarbot--issue-choose (match-point)
   "Show/hide replacements candidates for MATCH-POINT.
-Return selected replacement index."
+Return selected replacement index or nil if MATCH-POINT ignored."
   (if match-point
       (let* ((nreps (length (plist-get (cdr match-point) :replacements)))
              (read-prompt
@@ -232,21 +290,22 @@ Return selected replacement index."
       (delete-char (plist-get match :length)))))
 
 (defun grammarbot--issue-interactively (match-point)
-  "Interactively perform issue resolving for the MATCH-POINT."
+  "Interactively perform issue resolving for the MATCH-POINT.
+Return non-nil if issue has been replaced with one of the replacements."
   (goto-char (car match-point))
   (grammarbot--issue-highlight match-point)
 
   (when-let ((rep-idx (grammarbot--issue-choose match-point)))
     (grammarbot--issue-replace match-point rep-idx)))
 
-(defun grammarbot--plist-part-p (part plist)
+(defun grammarbot--plist-part-p (plist part)
   "Return non-nil PART is the structural part of the PLIST."
   (let ((part-alist (json--plist-to-alist part)))
     (cl-every (lambda (part-elem)
                 (let ((part-val (cdr part-elem))
                       (plist-val (plist-get plist (car part-elem))))
                   (cond ((and (listp part-val) (listp plist-val))
-                         (grammarbot--plist-part-p part-val plist-val))
+                         (grammarbot--plist-part-p plist-val part-val))
                         (t (equal part-val plist-val)))))
               part-alist)))
 
@@ -258,8 +317,22 @@ See `grammarbot-accept-single-choice-rules'."
     (and (= 1 (length (plist-get match :replacements)))
          grammarbot-accept-single-choice-rules
          (cl-some (apply-partially 'grammarbot--plist-part-p
-                                   grammarbot-accept-single-choice-rules)
-                  match))))
+                                   (plist-get match :rule))
+                  grammarbot-accept-single-choice-rules))))
+
+(defun grammarbot--issue-ignore-p (match-point)
+  "Return non-nil if MATCH-POINT should be automatically ignored.
+Only issues with single (or zero) choice replacements are ignored.
+See `grammarbot-ignore-single-choice-rules'."
+  (let ((match (cdr match-point)))
+    (and (< (length (plist-get match :replacements)) 2)
+         grammarbot-ignore-single-choice-rules
+         (cl-some (apply-partially 'grammarbot--plist-part-p
+                                   (plist-get match :rule))
+                  grammarbot-ignore-single-choice-rules))))
+
+(defsubst grammarbot--stat-inc (stat which)
+  (plist-put stat which (1+ (plist-get stat which))))
 
 ;;;###autoload
 (defun grammarbot (start end &optional arg)
@@ -282,20 +355,35 @@ the first suggestion, i.e. grammarbot in batch mode."
                     (cons (copy-marker
                            (+ start (plist-get match :offset)) t)
                           match))
-                  (plist-get result :matches))))
+                  (plist-get result :matches)))
+         (stats '(:ignored 0 :replaced 0)))
     (if match-points
         (unwind-protect
             (save-excursion
               (seq-doseq (match-pnt match-points)
-                (if (or arg (grammarbot--issue-accept-p match-pnt))
-                    (grammarbot--issue-replace match-pnt 0)
-                  (grammarbot--issue-interactively match-pnt))))
+                (cond ((or arg (grammarbot--issue-accept-p match-pnt))
+                       (grammarbot--issue-replace match-pnt 0)
+                       (grammarbot--stat-inc stats :replaced))
+
+                      ((grammarbot--issue-ignore-p match-pnt)
+                       (grammarbot--stat-inc stats :ignored))
+
+                      (t
+                       (if (grammarbot--issue-interactively match-pnt)
+                           (grammarbot--stat-inc stats :replaced)
+                         (grammarbot--stat-inc stats :ignored))))))
 
           (grammarbot--issue-highlight nil)
-          (grammarbot--issue-choose nil))
+          (grammarbot--issue-choose nil)
+          (when grammarbot-show-stats
+            (message "GrammarBot issues: %d, ignored: %d, replaced: %d"
+                     (length match-points)
+                     (plist-get stats :ignored)
+                     (plist-get stats :replaced))))
 
-      (message "GrammarBot v%s detected no errors"
-               (plist-get (plist-get result :software) :version)))))
+      (when grammarbot-show-stats
+        (message "GrammarBot v%s detected no issues"
+                 (plist-get (plist-get result :software) :version))))))
 
 (provide 'grammarbot)
 
